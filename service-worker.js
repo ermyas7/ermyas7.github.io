2e32ac48bfd70635f988fd5946428767742f75ea
ermyas7 <ermyas.fekadu@gmail.com>
2019-06-23 15:57:40 +0530

    Add root to cache

--- service-worker.js
+++ service-worker.js
@@ -1,53 +1,60 @@
 ///////////////////////// cache storage
 const cacheName = 'v2';
-// const cacheAssets = [
-//   'index.html',
-//   '/js/main.js',
-//   '/css/icon-font.css',
-//   '/css/style.css',
-//   '/img'
-// ];
+const cacheAssets = [
+  '/',
+  './index.html',
+  '/js/main.js',
+  '/css/icon-font.css',
+  '/css/style.css',
+  '/img'
+];
 
 ////////////// call install event on sw
 
-// Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log('Service Worker: Caching Files');
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
-// Call Activate Event
+/////// call activate event
+
 self.addEventListener('activate', e => {
-  console.log('Service Worker: Activated');
-  // Remove unwanted caches
+  console.log("Serviceworker Activated!");
+
+  //////////////// remove unwanted old cache
   e.waitUntil(
     caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== cacheName) {
-            console.log('Service Worker: Clearing Old Cache');
+      return Promise.all(cacheNames.map(
+        cache => {
+          if(cache!=cacheName){
+            console.log('service worker clearing old cache');
             return caches.delete(cache);
           }
-        })
-      );
+        }
+      ) )
     })
-  );
-});
+  )
+})
 
-// Call Fetch Event
+
+///////////////////////////////////////////////////////
+///////// call fetch event when no connection/////////
 self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // Make copy/clone of response
-        const resClone = res.clone();
-        // Open cahce
-        caches.open(cacheName).then(cache => {
-          // Add response to cache
-          cache.put(e.request, resClone);
-        });
-        return res;
-      })
-      .catch(err => caches.match(e.request).then(res => res))
-  );
-});
+  console.log('service worker fetching!');
+  e.respondWith(fetch(e.request)
+  .catch(() => caches.match(e.request)));
+})
+
+// self.addEventListener('fetch', e => {
+//   console.log('Service Worker: Fetching');
+//   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+// });
