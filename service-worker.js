6a6d67df165886e10534284b64cf1f1929584120
ermyas7 <ermyas.fekadu@gmail.com>
2019-06-23 13:44:26 +0530

    replace caching asset with clone

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,53 @@
 ///////////////////////// cache storage
 const cacheName = 'v2';
-const cacheAssets = [
-  'index.html',
-  '/js/main.js',
-  '/css/icon-font.css',
-  '/css/style.css',
-  '/img'
-];
+// const cacheAssets = [
+//   'index.html',
+//   '/js/main.js',
+//   '/css/icon-font.css',
+//   '/css/style.css',
+//   '/img'
+// ];
 
 ////////////// call install event on sw
 
+// Call Install Event
 self.addEventListener('install', e => {
   console.log('Service Worker: Installed');
+});
 
+// Call Activate Event
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  // Remove unwanted caches
   e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log('Service Worker: Caching Files');
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
-/////// call activate event
-
-self.addEventListener('activate', e => {
-  console.log("Serviceworker Activated!");
-
-  //////////////// remove unwanted old cache
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(cacheNames.map(
-        cache => {
-          if(cache!=cacheName){
-            console.log('service worker clearing old cache');
-            return caches.delete(cache);
-          }
-        }
-      ) )
-    })
-  )
-})
-
-
-///////////////////////////////////////////////////////
-///////// call fetch event when no connection/////////
+// Call Fetch Event
 self.addEventListener('fetch', e => {
-  console.log('service worker fetching!');
-  e.respondWith(fetch(e.request)
-  .catch(() => caches.match(e.request)));
-})
-
-// self.addEventListener('fetch', e => {
-//   console.log('Service Worker: Fetching');
-//   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-// });
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        // Make copy/clone of response
+        const resClone = res.clone();
+        // Open cahce
+        caches.open(cacheName).then(cache => {
+          // Add response to cache
+          cache.put(e.request, resClone);
+        });
+        return res;
+      })
+      .catch(err => caches.match(e.request).then(res => res))
+  );
+});
